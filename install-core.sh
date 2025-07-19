2a3c0139266f1669302335e38f6ea4d096da58c2
smk <mathieusalhi@yahoo.fr>
2025-07-19 19:06:34 +0200

    Update install-core.sh

--- install-core.sh
+++ install-core.sh
@@ -4,7 +4,7 @@ echo -e "\n📦 Mise à jour du système..."
 sudo apt update
 
 echo -e "\n📦 Installation des packages..."
-sudo apt install -y git cmake g++ wget build-essential libcurl4-openssl-dev ccache cmatrix tmux
+sudo apt install -y git cmake wget build-essential libcurl4-openssl-dev ccache cmatrix tmux
 
 echo -e "\n📥 Clonage du modèle llama.cpp..."
 git clone https://github.com/ggerganov/llama.cpp.git || {
